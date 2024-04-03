4bccd39c2f605aa4eb1665e556e08977bdcc88be
Oliver Goldstein <oliein@OUCL13830.local>
2024-04-03 23:05:52 +0100

    Inference and Molecule data type

---
+++ Molecule.hs
@@ -0,0 +1,82 @@
+import qualified Data.Vector as V
+import Control.Monad
+import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Sampler
+import Control.Monad.Bayes.Traced.Basic
+import Control.Monad.Bayes.Weighted
+import Data.List (sort)
+import Numeric.Log( Log( Exp ), ln )
+
+data Molecule = Molecule {atoms :: [Atom], bonds :: [Bond]} deriving (Eq, Read, Show)
+
+data Bond = Delocalised Integer [(Atom, Atom, BondType, BondLength)]
+          | Bond (Atom, Atom, BondType, BondLength)
+          deriving (Eq, Read, Show)
+
+newtype InductiveMolecule = InitialAtom ExtendedAtom
+
+data ExtendedAtom = EAtom {atom :: Atom, bondList :: [InductiveBond]}
+
+data InductiveBond = DelocalisedI Integer ([ExtendedAtom], BondType, EquilibriumBondLength)
+                    | InductiveBond (ExtendedAtom, BondType, EquilibriumBondLength)
+
+data Atom = Atom {
+    atomId                   :: Integer,
+    atomicSpec               :: ElementAttributes, 
+    coordinate               :: (Double, Double, Double),
+    electronConfig           ::
+    -- orbital                  :: MonadSample m => () -> m Double
+  } deriving (Eq, Read, Show)
+
+data BondType = HydrogenBond 
+              | CovalentBond {bondOrder :: Integer}  
+              | IonicBond deriving (Eq, Read, Show)
+
+newtype Angstrom = Angstrom Double deriving (Read, Show, Eq)
+
+type EquilibriumBondLength = Angstrom
+
+data AtomicSymbol = O | H | N | C | P | S | Cl | B | Fe deriving (Eq, Read, Show)
+
+data ElementAttributes = ElementAttributes
+  { symbol :: AtomicSymbol,
+    atomicNumber :: Integer,
+    atomicWeight :: Double
+  } deriving (Eq, Read, Show)
+
+abundances :: V.Vector Double
+abundances = V.fromList [0.49, 0.26, 0.03, 0.01, 0.008, 0.006, 0.004, 0.002, 0.001]
+-- O (Oxygen): 0.49
+-- H (Hydrogen): 0.26
+-- N (Nitrogen): 0.03
+-- C (Carbon): 0.01
+-- P (Phosphorus): 0.008
+-- S (Sulfur): 0.006
+-- Cl (Chlorine): 0.004
+-- B (Boron): 0.002
+-- Fe (Iron): 0.001
+
+elementAttributes :: AtomicSymbol -> ElementAttributes
+elementAttributes O = ElementAttributes O 8 15.999 
+elementAttributes H = ElementAttributes H 1 1.008 
+elementAttributes N = ElementAttributes N 7 14.007
+elementAttributes C = ElementAttributes C 6 12.011
+elementAttributes P = ElementAttributes P 15 30.974
+elementAttributes S = ElementAttributes S 16 32.065
+elementAttributes Cl = ElementAttributes Cl 17 35.453
+elementAttributes B = ElementAttributes B 5 10.811 3 
+elementAttributes Fe = ElementAttributes Fe 26 55.845
+
+priorElementAttributes :: MonadInfer m => m ElementAttributes
+priorElementAttributes = do
+  index <- categorical abundances
+  return $ case index of
+    0 -> elementAttributes O
+    1 -> elementAttributes H
+    2 -> elementAttributes N
+    3 -> elementAttributes C
+    4 -> elementAttributes P
+    5 -> elementAttributes S
+    6 -> elementAttributes Cl
+    7 -> elementAttributes B
+    8 -> elementAttributes Fe
---
+++ TestInference.hs
@@ -0,0 +1,61 @@
+import Control.Monad
+import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Sampler
+import Control.Monad.Bayes.Traced.Basic
+import Control.Monad.Bayes.Weighted
+import Data.List (sort)
+import Numeric.Log( Log( Exp ), ln )
+
+
+-- Define a simple model
+simpleModel :: MonadInfer m => Double -> m Double
+simpleModel threshold = do
+  sample <- normal 0 1
+  score (if ((sample > threshold) && (sample < threshold + 0.1)) then 100 else 0)
+  return sample
+
+
+
+
+-- Assuming simpleModel is compatible with MonadInfer
+simpleModelTraced :: MonadInfer m => Double -> Traced m Double
+simpleModelTraced dbl = simpleModel dbl -- if simpleModel uses MonadInfer, it might be directly compatible
+
+runMH :: MonadInfer m => Int -> Double -> m [Double]
+runMH steps threshold = mh steps (simpleModelTraced threshold)
+
+
+
+
+
+
+main :: IO ()
+main = do
+  let steps = 100000     -- Number of steps for MH
+      threshold = 3.0  -- Threshold parameter for your model
+  
+  -- Execute the MH algorithm and print the results
+  results <- sampleIOfixed $ runWeighted $ runMH steps threshold
+  print results
+  print (sum (fst results) / fromIntegral (length (fst results)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sOrbital :: MonadSample m => Double -> m Double
+sOrbital threshold = do
+  sample <- normal 0 1
+  return sample
+--runMHs :: Int -> Double -> Weighted SamplerIO [Double]
+--runMHs steps threshold = mh steps (simpleModel threshold)
