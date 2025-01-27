9cf29316dccc66639917f14185b7525bd1309fe9
Oliver J Goldstein <10864811+oliverjgoldstein@users.noreply.github.com>
2025-01-27 21:46:48 +0000

    Update Test.hs

--- Test.hs
+++ Test.hs
@@ -8,4 +8,4 @@ test2 :: IO ()
 test2 = do
     let db1FilePath = "./molecules/benzene.sdf"
     db1Molecules <- parseDB1File db1FilePath
-    writeMoleculeToFile "./molecules/" (fst . head $ dblMolecules)
+    writeMoleculeToFile "./molecules/" (fst . head $ db1Molecules)
